57abd7fca615aac26942905ee3376b3eb78e0050
shameljij <shamel.jij@gmail.com>
2024-06-07 21:30:12 +0200

    Add project files.

---
+++ itforall4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace itforall4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A = 2;
+            int B = 5;
+            int sum = 0;
+            int C = 20;
+
+            for (int i = A; i <= B; i++) // i 1 ====> 5
+            {
+                Console.WriteLine("Vor    Ausgabe -- Sum: " + sum + "| A: " + A + "| B: " + B + "| i: " + i);
+                sum = sum + i;//inkrementieren
+                Console.WriteLine("Danach Ausgabe -- Sum: " + sum +"| A: "+ A + "| B: "+ B + "| i: "+ i);
+            }
+
+            for (int x = 0; x < 5; x++) 
+            {
+                int Wert = 200034 - 2234;
+                Console.WriteLine("Wert von Variabel Wert: "+Wert);
+                Console.WriteLine("C von For Schleife: " + C);
+            }
+            Console.WriteLine("Ausgabe: "+sum);
+        }
+    }
+}
